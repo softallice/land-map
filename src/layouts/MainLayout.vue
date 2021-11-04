d6a6080bedf1b6cdfad674d61a156b9eaeb1ee1a
s991045 <s991045@gmail.com>
2021-11-04 17:18:13 +0900

    map

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <!-- <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        /> -->
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    
+    <q-page-container>
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+          
+        </keep-alive>
+      </router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+
+    return {
+    }
+  }
+})
+</script>
---
+++ src/pages/Map/Maps.vue
@@ -0,0 +1,47 @@
+<template>
+  <naver-maps
+    width="800px"
+    height="800px"
+    :mapOptions="mapOptions"
+    :initLayers="initLayers"
+    @onLoad="onLoadMap($event)"
+  >
+  </naver-maps>
+</template>
+
+<script>
+import { ref } from "vue";
+import { NaverMaps } from "vue3-naver-maps";
+
+export default {
+  components: { NaverMaps },
+  setup: () => {
+    const map = ref();
+    const mapOptions = {
+      latitude: 37.56663888630603, // 지도 중앙 위도
+      longitude: 126.97838310403904, // 지도 중앙 경도
+      zoom: 16,
+      zoomControl: false,
+      zoomControlOptions: { position: "TOP_RIGHT" },
+    };
+    const initLayers = [
+      "BACKGROUND",
+      "BACKGROUND_DETAIL",
+      "POI_KOREAN",
+      "TRANSIT",
+      "ENGLISH",
+    ];
+
+    const onLoadMap = (mapObject) => {
+      map.value = mapObject;
+    };
+
+    return {
+      map,
+      mapOptions,
+      initLayers,
+      onLoadMap,
+    };
+  },
+};
+</script>
